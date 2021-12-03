d9f464fc42a70ef1365dd70aa3d75019c0f91b10
Krzysztof Łazarz <krzysztof.lazarz.dev@gmail.com>
2021-12-03 22:59:20 +0100

    Project inserted

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CSV_to_Aiken
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.aikenTextBox = new System.Windows.Forms.RichTextBox();
+            this.infoBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // aikenTextBox
+            // 
+            this.aikenTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.aikenTextBox.Location = new System.Drawing.Point(133, 12);
+            this.aikenTextBox.Name = "aikenTextBox";
+            this.aikenTextBox.ReadOnly = true;
+            this.aikenTextBox.Size = new System.Drawing.Size(655, 428);
+            this.aikenTextBox.TabIndex = 1;
+            this.aikenTextBox.Text = "";
+            // 
+            // infoBox
+            // 
+            this.infoBox.Location = new System.Drawing.Point(12, 75);
+            this.infoBox.Name = "infoBox";
+            this.infoBox.Size = new System.Drawing.Size(115, 363);
+            this.infoBox.TabIndex = 2;
+            this.infoBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.infoBox);
+            this.Controls.Add(this.aikenTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox aikenTextBox;
+        private System.Windows.Forms.RichTextBox infoBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace CSV_to_Aiken
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Some obvious stuff - get file name and load its' content to CsvReader
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+            ofd.Multiselect = false;
+            DialogResult dr=ofd.ShowDialog();
+            if (dr != DialogResult.OK) return;
+            CsvImporter csv = new CsvImporter(File.ReadAllText(ofd.FileName));
+            
+            csv.next();     //we need to skip the header
+            string aiken = "";
+            int questions = 0;
+            while (csv.next())
+            {
+                if (csv.GetColumnCount() < 4) continue;     //question must contain question, min.2 answers, correct answer
+                int answerColumn = csv.GetColumnCount()-1;  //there might be a column with comemnts
+                while (answerColumn > 0 && csv.GetColumnValue(answerColumn).Length!=1)  //so we look for a column with one letter
+                    answerColumn--;
+                if (answerColumn <= 0) continue;
+
+                //Now we now that the row is a question
+                questions++;
+                aiken += csv.GetColumnValue(0) + '\n';
+                char optionLetter = 'A';
+                for (int i = 1; i < answerColumn; i++)
+                {
+                    aiken += optionLetter++;
+                    aiken += ". " + csv.GetColumnValue(i)+'\n';
+                }
+                aiken += "ANSWER: " + csv.GetColumnValue(answerColumn) + "\n\n";
+            }
+            aikenTextBox.Text = aiken;
+            infoBox.Text += "No. of questions:\n" + questions.ToString()+"\n";
+        }
+    }
+    public class CsvImporter
+    {
+        int dataPtr = -1;
+        string[] data = null;
+        string[] content = null;
+        public CsvImporter(string csv)
+        {
+            //csv.Split()
+            data = csv.Split('\n');
+            //data = csv.Split("," + "(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+        }
+        const string pattern = @"""\s*,\s*""";
+        public bool next()
+        {
+            dataPtr++;
+            if(dataPtr>=data.Length)
+                return false;
+
+            content = SplitCsvLine(data[dataPtr]).ToArray();
+            return true;
+        }
+        public int GetColumnCount()
+        {
+            return content.Length;
+        }
+        public string GetColumnValue(int column)
+        {
+            return content[column];
+        }
+        public List<string> SplitCsvLine(string s)
+        {
+            int i;
+            int a = 0;
+            int count = 0;
+            List<string> str = new List<string>();
+            for (i = 0; i < s.Length; i++)
+            {
+                switch (s[i])
+                {
+                    case ',':
+                        if ((count & 1) == 0)
+                        {
+                            str.Add(s.Substring(a, i - a));
+                            a = i + 1;
+                        }
+                        break;
+                    case '"':
+                    case '\'': count++; break;
+                }
+            }
+            str.Add(s.Substring(a));
+            str.RemoveAt(str.Count() - 1);
+            return str;
+        }
+    }
+}
